de6b069ce9bd13bc0aa35844807f1f3b83ac8b94
MyArch.ins <mr.dongsuibian@gmail.com>
2023-11-26 14:51:22 +0800

    换个字体
    1. Sparse Mono 字体
    2. jetbrains Mono 字体

--- config.h
+++ config.h
@@ -5,6 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
+// static char *font = "Sarasa Mono Slab CL Nerd Font:pixelsize=24:antialias=true:autohint=true";
 static char *font = "JetBrainsMono Nerd Font:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 2;
 
